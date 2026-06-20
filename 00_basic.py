588a79a0ad7b89eb68ff8f7e0ec141074a6c600d
nidhikalburgi23@gmail.com <nidhikalburgi23@gmail.com>
2026-06-20 20:16:16 +0530

    biopython

--- 00_basic.py
+++ 00_basic.py
@@ -1,3 +1,5 @@
 from Bio.Seq import Seq
 my_seq=Seq("AGTCACACTGGT")
-print(my_seq)
+print(my_seq) # o/p : AGTCACACTGGT
+print(my_seq.complement()) # o/p : TCATGTGACCA
+print(my_seq.reverse_complement()) # o/p : ACCAGTGTACT
